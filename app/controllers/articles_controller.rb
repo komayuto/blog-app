aa91b750c76a77dae784946a982531454ed38714
古俣祐斗 <komayuto1999@gmail.com>
2020-09-25 07:22:33 +0900

    記事の更新 - update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+
+  end
+
   # データを保存する際に使う
   private
   def article_params
