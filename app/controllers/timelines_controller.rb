f83ab08176e6a8116872fc89995f6d7001f2476f
古俣祐斗 <komayuto1999@gmail.com>
2020-10-21 21:28:30 +0900

    タイムラインの簡単な実装まで完了

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,12 @@
+class TimelinesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def show
+    user_ids = current_user.followings.pluck(:id)
+    # current_userでログインしているユーザー。followingsでログインしているユーザーのフォローしているユーザー。pluck(:id)でフォローしているの全ての取得。
+    @articles = Article.where(user_id: user_ids)
+    # Article.whereで記事を探す。user_idがuser_idsで指定されているユーザーのIDを
+  end
+
+end
